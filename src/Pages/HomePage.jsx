73b5de23925439b5de51e9c3e9e3c29a8169bce7
Emiliano Foti <foti.emiliano@gmail.com>
2023-12-22 15:50:25 +0100

    homepage linked with server

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const API_URL = "http://localhost:5005";
 
 function HomePage() {
+    const [items, setItems] = useState([]);
+
+    const getAllItems = () => {
+        axios
+          .get(`${API_URL}/items`)
+          .then((response) => setItems(response.data))
+          .catch((error) => console.error(error));
+      };
+
+    useEffect(() => {
+        getAllItems();
+    }, [] );  
+
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="ItemsList">
+             {items.map((item) => {
+          return (
+            <div className="Item card" key={item.id} >
+              <Link to={`/items/${item.id}`}>
+                <h3>{item.name}</h3>
+                <img src={item.picture} alt="item picture"/> 
+                <p>{item.description}</p>
+                <p>{item.price}€</p>
+              </Link>
+            </div>
+            );
+            })} 
         </div>
     )
 }
