ace72d4108d7b77927dec5fb5369a03952dbdf31
Janos Sitku <sitku.janos86@gmail.com>
2024-01-05 18:17:16 +0100

    filter done

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -3,50 +3,45 @@ import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 function DetailsPage() {
-    const [item, setItem] = useState({});
-    const {itemId} = useParams();
+  const [item, setItem] = useState({});
+  const { itemId } = useParams();
 
-    const navigate = useNavigate();
-    const API_URL = "http://localhost:5005";
-          
-    const getSingleItem = () => {
-        axios
-            .get(`${API_URL}/items/${itemId}`)
-            .then((response) => setItem(response.data))
-            .catch((error) => console.error(error));
-    };
-    
-    useEffect(() => {
-            getSingleItem();
-    }, [itemId] );  
+  const navigate = useNavigate();
+  const API_URL = "http://localhost:5005";
 
-    return (
-        <div className="itemCtn">
-            {item && (
-            <>
-                <img
-                    src={item.picture}
-                    alt="Item Image"
-                    height="30px"
-                    width="auto"
-                />
-                <h3>{item.name}</h3>
-                <p>Price: {item.price}</p>
-                <p>Description: {item.description}</p>
-                <p>Category: {item.category}</p>
-        
-                <button
-                    className="itemBnt"
-                    onClick={() => {
-                    navigate("/");
-                    }}
-                >
-                    Back
-                </button>
-            </>
-            )}
-        </div>
-        );
-    }
-        
+  const getSingleItem = () => {
+    axios
+      .get(`${API_URL}/items/${itemId}`)
+      .then((response) => setItem(response.data))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getSingleItem();
+  }, [itemId]);
+
+  return (
+    <div className="itemCtn">
+      {item && (
+        <>
+          <img src={item.picture} alt="Item Image" height="30px" width="auto" />
+          <h3>{item.name}</h3>
+          <p>Price: {item.price}</p>
+          <p>Description: {item.description}</p>
+          <p>Category: {item.category}</p>
+
+          <button
+            className="itemBnt"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Back
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
+
 export default DetailsPage;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,58 +2,53 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Search from "../Components/Search";
-
-let API_URL = "http://localhost:5005";
-
 function HomePage() {
-    const [items, setItems] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('')
-    const [timeoutId, setTimeoutId] = useState()
-
-    const getAllItems = (query) => {
-      if (query) {
-        API_URL += `/items/search?q=${query}`
-      }
-      axios
-          .get(`${API_URL}/items`)
-          .then((response) => setItems(response.data))
-          .catch((error) => console.error(error));
-      };
-
-    useEffect(() => {
-        getAllItems();
-    }, [] );  
-
-    useEffect(() => {
-      clearTimeout(timeoutId)
-      if (searchTerm) {
-        setTimeoutId(
-          setTimeout(() => {
-            getAllItems(searchTerm)
-          }, 500)
-        )
-      }
-    }, [searchTerm])
-
-    return (
-      <>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-        <div className="ItemsList">
-             {items.map((item) => {
+  let API_URL = "http://localhost:5005/items";
+  const [items, setItems] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [timeoutId, setTimeoutId] = useState();
+  const getAllItems = (query) => {
+    if (query) {
+      API_URL += `?q=${query}`;
+    }
+    axios
+      .get(`${API_URL}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => console.error(error));
+  };
+  useEffect(() => {
+    getAllItems();
+  }, []);
+  useEffect(() => {
+    clearTimeout(timeoutId);
+    if (searchTerm) {
+      setTimeoutId(
+        setTimeout(() => {
+          getAllItems(searchTerm);
+        }, 300)
+      );
+    } else {
+      getAllItems();
+    }
+  }, [searchTerm]);
+  return (
+    <>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="ItemsList">
+        {items.map((item) => {
           return (
-            <div className="Item card" key={item.id} >
+            <div className="Item card" key={item.id}>
               <Link to={"/item/" + item.id}>
                 <h3>{item.name}</h3>
-                <img src={item.picture} alt="item picture"/> 
+                <img src={item.picture} alt="item picture" />
                 <p>{item.description}</p>
                 <p>{item.price}€</p>
               </Link>
             </div>
-            );
-            })} 
-        </div>
-      </>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 }
-
 export default HomePage;
