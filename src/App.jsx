f839dee8d9a719bea6cd0e1bad70c3e0d4b6bc4c
Viviane Huang <vivianehuang2022@gmail.com>
2024-01-08 15:08:15 +0100

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,57 +11,48 @@ import QuotePage from "./Pages/QuotePage";
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import EditItemPage from "./Pages/EditItemPage";
+import { AppShell } from "@mantine/core";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
   
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <HomePage cartItems={cartItems} setCartItems={setCartItems} />
-          }
-        />
-        <Route path="/About" element={<AboutPage />} />
-        <Route
-          path="/Cart"
-          element={
-            <CartPage cartItems={cartItems} setCartItems={setCartItems} />
-          }
-        />
-        <Route path="/AddItem" element={<AddItemPage />} />
-        <Route path="/Quote" element={<QuotePage />} />
-        <Route path="/item/:itemId" element={<DetailsPage />} />
-        <Route path="/edit/:itemId" element={<EditItemPage />} />
-        <Route path="*" element={<h1>404 Page</h1>} />
-      </Routes>
-      <Footer />
-
+      
       <AppShell
       header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
     >
       <AppShell.Header>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
-        <div>Logo</div>
+        <Navbar />        
       </AppShell.Header>
 
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      <AppShell.Main>      
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <HomePage cartItems={cartItems} setCartItems={setCartItems} />
+            }
+          />
+          <Route path="/About" element={<AboutPage />} />
+          <Route
+            path="/Cart"
+            element={
+              <CartPage cartItems={cartItems} setCartItems={setCartItems} />
+            }
+          />
+          <Route path="/AddItem" element={<AddItemPage />} />
+          <Route path="/Quote" element={<QuotePage />} />
+          <Route path="/item/:itemId" element={<DetailsPage />} />
+          <Route path="/edit/:itemId" element={<EditItemPage />} />
+          <Route path="*" element={<h1>404 Page</h1>} />
+        </Routes>
 
-      <AppShell.Main>Main</AppShell.Main>
+        <AppShell.Footer > 
+          <Footer />
+        </AppShell.Footer>
+
+      </AppShell.Main>
     </AppShell>
 
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom"; 
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
