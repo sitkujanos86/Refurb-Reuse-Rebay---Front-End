175703229f6174389072fb538325a5194c725a0f
Viviane Huang <vivianehuang2022@gmail.com>
2024-01-08 17:10:11 +0100

    notification function added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Search from "../Components/Search";
 import { Card, Image, Text, Button, Group } from '@mantine/core';
 import { SimpleGrid } from '@mantine/core';
+import { notifications } from '@mantine/notifications';
 
 function HomePage({cartItems, setCartItems}) {
 
@@ -41,7 +42,6 @@ function HomePage({cartItems, setCartItems}) {
 
   const addToCart = (item) => {
       setCartItems([...cartItems, item]);
-      window.alert('Item added to the cart!')
   };
 
   const deleteItem = (itemId) => { 
@@ -88,9 +88,19 @@ function HomePage({cartItems, setCartItems}) {
                           {item.price}€
                         </Text>
                         </Link>
-                        <Button color="blue" fullWidth mt="md" radius="md" onClick={() => addToCart(item)}>
+                        
+      
+    
+                        <Button color="blue" fullWidth mt="md" radius="md" onClick={() => {
+                          notifications.show({
+                            title: 'Item added to the cart!'
+                          });
+                          addToCart(item)}}
+                          >
                         Add to Cart
                         </Button>
+                        
+
                         <Button color="blue" fullWidth mt="md" radius="md" onClick={() => deleteItem(item.id)}>
                         Delete item
                         </Button>
