dab173cf5bbe8b072c5ce5c8534114047e804f78
Emiliano Foti <foti.emiliano@gmail.com>
2024-02-08 18:52:34 +0100

    updated navbar with menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,69 +1,41 @@
-import { Button, Menu, ThemeIcon } from "@mantine/core";
+import { Button, Menu, Text, ThemeIcon } from "@mantine/core";
 import "../Styles/Navbar.css";
 import { Link } from "react-router-dom";
 import { IconShoppingCart } from "@tabler/icons-react";
 import rEbay from "../assets/images/Rebay Logo.png";
 import { SimpleGrid } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
-import { useState } from "react";
 
 function Navbar() {
   const { width } = useViewportSize();
-  const [opened, setOpened] = useState(false);
 
   return width > 1200 ? (
     <>
-      <SimpleGrid ml="13vw" cols={5}>
+      <SimpleGrid ml="5vw" cols={5}>
         <Link to="/">
           <img className="logo" src={rEbay} alt="ReBay" />
         </Link>
 
         <Link to="/About">
-          <Button color="blue" mt="2.1rem" radius="md">
-            About US
-          </Button>
+          <Text mt="2.2rem" fw={500} ta="center">
+            About Us
+          </Text>
         </Link>
 
         <Link to="/AddItem">
-          <Button color="blue" mt="2.1rem" radius="md">
-            Add Item
-          </Button>
+          <Text mt="2.2rem" fw={500} ta="center">
+            Add item
+          </Text>
         </Link>
 
         <Link to="/Quote">
-          <Button color="blue" mt="2.1rem" radius="md">
+          <Text mt="2.2rem" fw={500} ta="center">
             Ask for quote
-          </Button>
+          </Text>
         </Link>
 
         <Link to="/Cart">
-          <ThemeIcon radius="md" size="lg" color="orange" mt="2.1rem">
-            <IconShoppingCart size="2rem" stroke={1.5} />
-          </ThemeIcon>
-        </Link>
-      </SimpleGrid>
-    </>
-  ) : width > 800 ? (
-    <>
-      <SimpleGrid ml="5vw" cols={4}>
-        <Link to="/">
-          <img className="logo" src={rEbay} alt="ReBay" />
-        </Link>
-
-        <Link to="/AddItem">
-          <Button color="blue" ml="3rem" mt="2.1rem" radius="md">
-            Add Item
-          </Button>
-        </Link>
-
-        <Link to="/Quote">
-          <Button color="blue" ml="2rem" mt="2.1rem" radius="md">
-            Ask for quote
-          </Button>
-        </Link>
-
-        <Link to="/Cart">
-          <ThemeIcon radius="md" size="lg" color="orange" ml="4rem" mt="2.1rem">
+          <ThemeIcon radius="md" size="lg" color="orange" mt="2.1rem" ml="6rem">
             <IconShoppingCart size="2rem" stroke={1.5} />
           </ThemeIcon>
         </Link>
@@ -71,29 +43,44 @@ function Navbar() {
     </>
   ) : (
     <>
-      <SimpleGrid ml="5vw" cols={2}>
-        <Menu opened={opened} onChange={setOpened} shadow="md" width={200}>
+      <SimpleGrid ml="5vw" cols={3}>
+        <Link to="/">
+          <img className="logoSm" src={rEbay} alt="ReBay" />
+        </Link>
+        <Menu
+          trigger="click-hover"
+          openDelay={100}
+          closeDelay={400}
+          shadow="md"
+          width={150}
+        >
           <Menu.Target>
-            <Link to="/">
-              <img className="logo" src={rEbay} alt="ReBay" />
-            </Link>
+            <Text mt="1.5rem" fw={500} ta="center">
+              Menu
+            </Text>
           </Menu.Target>
           <Menu.Dropdown>
-            <Link to="/AddItem">
-              <Button color="blue" ml="3rem" mt="2.1rem" radius="md">
-                Add Item
-              </Button>
+            <Link to="/About">
+              <Text mt="1rem" fw={500} ta="center">
+                About Us
+              </Text>
             </Link>
-
+            <Menu.Divider />
+            <Link to="/AddItem">
+              <Text mt="1rem" fw={500} ta="center">
+                Add item
+              </Text>
+            </Link>
+            <Menu.Divider />
             <Link to="/Quote">
-              <Button color="blue" ml="2rem" mt="2.1rem" radius="md">
+              <Text my="1rem" fw={500} ta="center">
                 Ask for quote
-              </Button>
+              </Text>
             </Link>
           </Menu.Dropdown>
         </Menu>
         <Link to="/Cart">
-          <ThemeIcon radius="md" size="lg" color="orange" ml="30vw" mt="2.1rem">
+          <ThemeIcon radius="md" size="lg" color="orange" ml="15vw" mt="1.3rem">
             <IconShoppingCart size="2rem" stroke={1.5} />
           </ThemeIcon>
         </Link>
