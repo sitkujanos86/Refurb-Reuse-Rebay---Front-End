510c65a79715f19853424c3d6e6864e32859696c
Emiliano Foti <foti.emiliano@gmail.com>
2024-02-09 15:37:58 +0100

    added a burger in mobile view

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,9 +5,12 @@ import { IconShoppingCart } from "@tabler/icons-react";
 import rEbay from "../assets/images/Rebay Logo.png";
 import { SimpleGrid } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
+import { useDisclosure } from "@mantine/hooks";
+import { Burger } from "@mantine/core";
 
 function Navbar() {
   const { width } = useViewportSize();
+  const [opened, { toggle }] = useDisclosure();
 
   return width > 1200 ? (
     <>
@@ -35,7 +38,7 @@ function Navbar() {
         </Link>
 
         <Link to="/Cart">
-          <ThemeIcon radius="md" size="lg" color="orange" mt="2.1rem" ml="6rem">
+          <ThemeIcon radius="md" size="lg" color="orange" mt="2.1rem" ml="7rem">
             <IconShoppingCart size="2rem" stroke={1.5} />
           </ThemeIcon>
         </Link>
@@ -43,44 +46,42 @@ function Navbar() {
     </>
   ) : (
     <>
-      <SimpleGrid ml="5vw" cols={3}>
-        <Link to="/">
-          <img className="logoSm" src={rEbay} alt="ReBay" />
-        </Link>
-        <Menu
-          trigger="click-hover"
-          openDelay={100}
-          closeDelay={400}
-          shadow="md"
-          width={150}
-        >
-          <Menu.Target>
-            <Text mt="1.5rem" fw={500} ta="center">
-              Menu
-            </Text>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Link to="/About">
-              <Text mt="1rem" fw={500} ta="center">
-                About Us
-              </Text>
-            </Link>
-            <Menu.Divider />
-            <Link to="/AddItem">
-              <Text mt="1rem" fw={500} ta="center">
-                Add item
-              </Text>
-            </Link>
-            <Menu.Divider />
-            <Link to="/Quote">
-              <Text my="1rem" fw={500} ta="center">
-                Ask for quote
-              </Text>
-            </Link>
-          </Menu.Dropdown>
-        </Menu>
+      <SimpleGrid ml="5vw" cols={2}>
+        <div className="logoBurger">
+          <Menu shadow="md" width={150}>
+            <Menu.Target>
+              <Burger
+                opened={opened}
+                onClick={toggle}
+                aria-label="Toggle navigation"
+              />
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Link to="/About">
+                <Text mt="1rem" fw={500} ta="center">
+                  About Us
+                </Text>
+              </Link>
+              <Menu.Divider />
+              <Link to="/AddItem">
+                <Text mt="1rem" fw={500} ta="center">
+                  Add item
+                </Text>
+              </Link>
+              <Menu.Divider />
+              <Link to="/Quote">
+                <Text my="1rem" fw={500} ta="center">
+                  Ask for quote
+                </Text>
+              </Link>
+            </Menu.Dropdown>
+          </Menu>
+          <Link to="/">
+            <img className="logoSm" src={rEbay} alt="ReBay" />
+          </Link>
+        </div>
         <Link to="/Cart">
-          <ThemeIcon radius="md" size="lg" color="orange" ml="15vw" mt="1.3rem">
+          <ThemeIcon radius="md" size="lg" color="orange" ml="30vw" mt="1.3rem">
             <IconShoppingCart size="2rem" stroke={1.5} />
           </ThemeIcon>
         </Link>
